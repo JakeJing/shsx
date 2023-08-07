67b6c4a9d37151c7a2c609797be060b58725487e
JakeJing <350956009@qq.com>
2023-08-07 23:39:26 +0800

    update script for python3

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -26,22 +26,22 @@ libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.18-RE
 cur_dir=`pwd`
 # Stream Ciphers
 ciphers=(
-aes-256-gcm
-aes-192-gcm
-aes-128-gcm
-aes-256-ctr
-aes-192-ctr
-aes-128-ctr
-aes-256-cfb
-aes-192-cfb
-aes-128-cfb
-camellia-128-cfb
-camellia-192-cfb
-camellia-256-cfb
-chacha20-ietf-poly1305
-chacha20-ietf
-chacha20
-rc4-md5
+    aes-256-gcm
+    aes-192-gcm
+    aes-128-gcm
+    aes-256-ctr
+    aes-192-ctr
+    aes-128-ctr
+    aes-256-cfb
+    aes-192-cfb
+    aes-128-cfb
+    camellia-128-cfb
+    camellia-192-cfb
+    camellia-256-cfb
+    chacha20-ietf-poly1305
+    chacha20-ietf
+    chacha20
+    rc4-md5
 )
 # Color
 red='\033[0;31m'
@@ -173,50 +173,50 @@ pre_install(){
     # Set shadowsocks config port
     while true
     do
-    dport=$(shuf -i 9000-19999 -n 1)
-    echo "Please enter a port for shadowsocks-python [1-65535]"
-    read -p "(Default port: ${dport}):" shadowsocksport
-    [ -z "$shadowsocksport" ] && shadowsocksport=${dport}
-    expr ${shadowsocksport} + 1 &>/dev/null
-    if [ $? -eq 0 ]; then
-        if [ ${shadowsocksport} -ge 1 ] && [ ${shadowsocksport} -le 65535 ] && [ ${shadowsocksport:0:1} != 0 ]; then
-            echo
-            echo "---------------------------"
-            echo "port = ${shadowsocksport}"
-            echo "---------------------------"
-            echo
-            break
+        dport=$(shuf -i 9000-19999 -n 1)
+        echo "Please enter a port for shadowsocks-python [1-65535]"
+        read -p "(Default port: ${dport}):" shadowsocksport
+        [ -z "$shadowsocksport" ] && shadowsocksport=${dport}
+        expr ${shadowsocksport} + 1 &>/dev/null
+        if [ $? -eq 0 ]; then
+            if [ ${shadowsocksport} -ge 1 ] && [ ${shadowsocksport} -le 65535 ] && [ ${shadowsocksport:0:1} != 0 ]; then
+                echo
+                echo "---------------------------"
+                echo "port = ${shadowsocksport}"
+                echo "---------------------------"
+                echo
+                break
+            fi
         fi
-    fi
-    echo -e "[${red}Error${plain}] Please enter a correct number [1-65535]"
+        echo -e "[${red}Error${plain}] Please enter a correct number [1-65535]"
     done
 
     # Set shadowsocks config stream ciphers
     while true
     do
-    echo -e "Please select stream cipher for shadowsocks-python:"
-    for ((i=1;i<=${#ciphers[@]};i++ )); do
-        hint="${ciphers[$i-1]}"
-        echo -e "${green}${i}${plain}) ${hint}"
-    done
-    read -p "Which cipher you'd select(Default: ${ciphers[0]}):" pick
-    [ -z "$pick" ] && pick=1
-    expr ${pick} + 1 &>/dev/null
-    if [ $? -ne 0 ]; then
-        echo -e "[${red}Error${plain}] Please enter a number"
-        continue
-    fi
-    if [[ "$pick" -lt 1 || "$pick" -gt ${#ciphers[@]} ]]; then
-        echo -e "[${red}Error${plain}] Please enter a number between 1 and ${#ciphers[@]}"
-        continue
-    fi
-    shadowsockscipher=${ciphers[$pick-1]}
-    echo
-    echo "---------------------------"
-    echo "cipher = ${shadowsockscipher}"
-    echo "---------------------------"
-    echo
-    break
+        echo -e "Please select stream cipher for shadowsocks-python:"
+        for ((i=1;i<=${#ciphers[@]};i++ )); do
+            hint="${ciphers[$i-1]}"
+            echo -e "${green}${i}${plain}) ${hint}"
+        done
+        read -p "Which cipher you'd select(Default: ${ciphers[0]}):" pick
+        [ -z "$pick" ] && pick=1
+        expr ${pick} + 1 &>/dev/null
+        if [ $? -ne 0 ]; then
+            echo -e "[${red}Error${plain}] Please enter a number"
+            continue
+        fi
+        if [[ "$pick" -lt 1 || "$pick" -gt ${#ciphers[@]} ]]; then
+            echo -e "[${red}Error${plain}] Please enter a number between 1 and ${#ciphers[@]}"
+            continue
+        fi
+        shadowsockscipher=${ciphers[$pick-1]}
+        echo
+        echo "---------------------------"
+        echo "cipher = ${shadowsockscipher}"
+        echo "---------------------------"
+        echo
+        break
     done
 
     echo
@@ -332,7 +332,7 @@ install(){
     fi
 
     cd ${cur_dir}/shadowsocks-master
-    python setup.py install --record /usr/local/shadowsocks_install.log
+    python3 setup.py install --record /usr/local/shadowsocks_install.log
 
     if [ -f /usr/bin/ssserver ] || [ -f /usr/local/bin/ssserver ]; then
         chmod +x /etc/init.d/shadowsocks
@@ -424,5 +424,5 @@ case "$action" in
     *)
         echo "Arguments error! [${action}]"
         echo "Usage: `basename $0` [install|uninstall]"
-    ;;
+        ;;
 esac
